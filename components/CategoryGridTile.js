daf5c6af0849b93cd5d12e7ce410e5bf6d468e4e
Pema Ngodup <pemangodup@Pemas-MBP.Home>
2025-09-19 17:29:33 +0100

    Categories screen grid layout

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,16 @@
-import { View, Text, Pressable, StyleSheet } from "react-native";
+import { View, Text, Pressable, StyleSheet, Platform } from "react-native";
 
 function CategoryGridTile({ title, color }) {
   return (
-    <View>
-      <Pressable>
-        <View style={{ backgroundColor: color }}>
-          <Text>{title}</Text>
+    <View style={styles.gridItem}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -15,7 +20,36 @@ function CategoryGridTile({ title, color }) {
 export default CategoryGridTile;
 
 const styles = StyleSheet.create({
-  hehe: {
-    backgroundColor: "red",
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  button: {
+    flex: 1,
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: null,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
   },
 });
