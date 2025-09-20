5e873cacceaac63584ee94e7ba511f099af9476a
Pema Ngodup <pemangodup@Pemas-MBP.Home>
2025-09-20 10:52:19 +0100

    We can also use useRoute as an alternative for route parameter

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,8 +1,12 @@
 import { View, Text, StyleSheet } from "react-native";
+// import { useRoute } from "@react-navigation/native";
 
 import { MEALS } from "../data/dummy-data";
 
 function MealsOverViewScreen({ route }) {
+  // Can also use useRoute hooks to get the parameter
+  // const route = useRoute();
+  // const catId = route.params.categoryId;
   const catId = route.params.categoryId;
   return (
     <View style={styles.container}>
