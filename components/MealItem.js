379f7923ab1a20a486fba95013180b75cd85b3a5
Pema Ngodup <pemangodup@Pemas-MBP.Home>
2025-09-23 18:42:53 +0100

    Finiishing the meal detail screen

--- components/MealItem.js
+++ components/MealItem.js
@@ -8,6 +8,7 @@ import {
 } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
+import MealDetails from "./MealDetails";
 
 function MealItem({
   id,
@@ -40,17 +41,13 @@ function MealItem({
         <View style={styles.innerContainer}>
           <View>
             <Image source={{ uri: imageUrl }} style={styles.image} />
-            <View>
-              <Text style={styles.title}>{title}</Text>
-            </View>
-            <View style={styles.details}>
-              <Text style={styles.detailItem}>{duration}</Text>
-              <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-              <Text style={styles.detailItem}>
-                {affordability.toUpperCase()}
-              </Text>
-            </View>
+            <Text style={styles.title}>{title}</Text>
           </View>
+          <MealDetails
+            duration={duration}
+            complexity={complexity}
+            affordability={affordability}
+          />
         </View>
       </Pressable>
     </View>
@@ -87,15 +84,5 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 18,
     margin: 8,
-  },
-  details: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 8,
-  },
-  detailItem: {
-    marginHorizontal: 4,
-    fontSize: 12,
   },
 });
